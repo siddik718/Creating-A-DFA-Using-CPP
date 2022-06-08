8323411947e157f40c808859109898c8f957dc53
Abu Bakar Siddik <70488808+siddik718@users.noreply.github.com>
2022-06-08 15:34:31 +0600

    Update DFA.cpp

--- DFA.cpp
+++ DFA.cpp
@@ -12,35 +12,32 @@ int main(){
     string start_state;
     set<string>finalstates;
     string input;
-    cout<<"Enter the number of states: ";
-    int n;cin >> n;
     cout << "Enter the states: ";
-    for(int i=0;i<n;i++){
-        string s;cin>>s;
-        states.push_back(s);
+    char str[2],ch=' ';
+    while(ch != '\n'){
+        scanf("%s%c",str,&ch);
+        states.push_back(str);
     }
-    cout<<"Enter the number of alphabet: ";
-    int m;cin>>m;
+    char c;ch=' ';
     cout<<"Enter the alphabet: ";
-    for(int i=0;i<m;i++){
-        char c;cin>>c;
+    while(ch != '\n'){
+        scanf("%c%c",&c,&ch);
         alphabet.push_back(c);
     }
-    cout<<"Enter the startstates: ";
+    cout<<"Enter the start_states: ";
     cin>>start_state;
-    cout<<"Enter the number of final states: ";
-    int M;cin>>M;
-    cout<<"Enter the final states: ";    
-    for(int i=0;i<M;i++){
-        string c;cin>>c;
-        finalstates.insert(c);
+    ch = ' ';
+    cout<<"Enter the Final_states: ";
+    while(ch != '\n'){
+        scanf("%s%c",str,&ch);
+        finalstates.insert(str);
     }
     cout<<"Fill the Transition Table: \n";
-    for(int i = 0;i<n;i++){
-        for(int j = 0;j<m;j++){
-            cout << "State "<<states[i]<<" moves for alphabet " << alphabet[j] << " to State : ";
+    for(auto state : states){
+        for(auto alpha : alphabet){
+            cout << "State "<<state<<" moves for alphabet " << alpha << " to State : ";
             string s;cin >> s;
-            transistates[{states[i],alphabet[j]}] = s;
+            transistates[{state,alpha}] = s;
         }
     }
     while(true){
@@ -50,7 +47,7 @@ int main(){
         int f = 1;
         for(int i = 0;i<input.length();i++){
             char ch = input[i];
-            if(transistates.find({current_state,ch}) == transistates.end()){
+            if(transistates.find({current_state,ch}) == transistates.end()){ // when we can't move to any state.
                 cout<<"No transition found for " << current_state << " " << ch << endl;
                 f = 0;
                 break;
